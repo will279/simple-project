91718353fb81fd46dc5c9062ddc294fb8300218d
Sergey A. Chebotarev <sachebotarev@gmail.com>
2015-03-05 00:18:16 +0300

    openui5_connect configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,46 +5,46 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 
 		dir: {
-			bower: 'bower_components',
-			webapp: 'webapp'
+			bower_components: 'bower_components', // Директория установки bower компонентов
+			webapp: 'webapp'					  // Директория с исходным кодом приложения
 		}
 	// 1. Конфигурирование задач
 		connect: {
 	    	options: {
-		        port: 8080,
-		        hostname: 'localhost',
-		        livereload: true
+		        port: 8080, 					  // Порт на котором будет доступен веб-сервер
+		        hostname: 'localhost',			  // Хост который будет использовать веб-сервер
+		        livereload: true				  // Активация livereload
 	    	},
 	    	src: {},
 		},
 
 		openui5_connect: {
 		    options: {
-		        resources: [
-		            'bower_components/openui5-sap.ui.core/resources',
-		            'bower_components/openui5-sap.m/resources',
-		            'bower_components/openui5-sap.ui.layout/resources',
-		            'bower_components/openui5-themelib_sap_bluecrystal/resources'
+		        resources: [					 // Путь к библиотекам OpenUI5
+		            '<%= dir.bower_components %>/openui5-sap.ui.core/resources',
+		            '<%= dir.bower_components %>/openui5-sap.m/resources',
+		            '<%= dir.bower_components %>/openui5-sap.ui.layout/resources',
+		            '<%= dir.bower_components %>/openui5-themelib_sap_bluecrystal/resources'
 		        ],
-		        proxypath: 'proxy'
+		        proxypath: 'proxy'				// Префикс сетевого адреса для передачи запроса через proxy-сервер
 		    },
     		src: {
 		        options: {
-		            appresources: 'webapp'
+		            appresources: '<%= dir.webapp %>'      // Директория с файлами веб-приложения
 		        }
     		},
 		}
 	})
 	// 2. Загрузка задач
-	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-connect');  // Загрузка плагинов
 	grunt.loadNpmTasks('grunt-openui5');
 
 	// 3. Регистрация задач (создание псевдонимов)
-	grunt.registerTask('serve', [
+	grunt.registerTask('serve', [					// Создание псевдонима для запуска задачи openui5_connect с параметром
 			'openui5_connect:src:keepalive'
 	]);
 
-	grunt.registerTask('default', [
+	grunt.registerTask('default', [					// Задача по умолчанию
     	'serve'
     ]);
 }
