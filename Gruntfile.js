0da08b4ebfda074ea4ae30d780efc7e25e33d594
Sergey A. Chebotarev <sachebotarev@gmail.com>
2015-03-04 23:44:43 +0300

    openui5_connect configuration

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,45 @@
 'use strict';
 
 module.exports = function(grunt) {
-	// 1. Загрузка задач
+	
 	grunt.initConfig({
-	// 2. Конфигурирование задач
+	// 1. Конфигурирование задач
+		connect: {
+	    	options: {
+		        port: 8080,
+		        hostname: 'localhost',
+		        livereload: true
+	    	},
+	    	src: {},
+		},
+
+		openui5_connect: {
+		    options: {
+		        resources: [
+		            'bower_components/openui5-sap.ui.core/resources',
+		            'bower_components/openui5-sap.m/resources',
+		            'bower_components/openui5-sap.ui.layout/resources',
+		            'bower_components/openui5-themelib_sap_bluecrystal/resources'
+		        ],
+		        proxypath: 'proxy'
+		    },
+    		src: {
+		        options: {
+		            appresources: 'webapp'
+		        }
+    		},
+		}
 	})
+	// 2. Загрузка задач
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-openui5');
+
 	// 3. Регистрация задач (создание псевдонимов)
+	grunt.registerTask('serve', [
+			'openui5_connect:src:keepalive'
+	]);
+
+	grunt.registerTask("default", [
+    	'serve'
+    ]);
 }
